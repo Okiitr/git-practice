530deb0e475b308cbdab6e8260688868878b5d05
Oman <kumaroman4@gmail.com>
2022-04-11 17:52:20 +0530

    new test

--- test.py
+++ test.py
@@ -3,11 +3,21 @@ import unittest
 def add(a,b):
     result= a+b
     return result
+def sub(a,b):
+    result= a-b
+    return result
+def mul(a,b):
+    result= a*b
+    return result
 
 class test (unittest.TestCase):
     
     def test_add(self):
-        self.assertEqual(add(5,10),14)
+        self.assertEqual(add(5,10),15)
+    def test_sub(self):
+        self.assertEqual(sub(15,10),5)
+    def test_add(self):
+        self.assertEqual(mul(5,10),50)
         
         
         
